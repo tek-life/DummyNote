a39254e932ab04b7e0a6524919ba8eb34706544b
Haifeng Li <hfli@marvell.com>
2013-12-22 16:19:47 +0800

    Commit for chapter 30

---
+++ src/com/example/dummynote/DummyNote.java
@@ -0,0 +1,33 @@
+package com.example.dummynote;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+public class DummyNote extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_dummy_note);
+		setAdapter();
+	}
+	
+	private String[] note_array={
+			"Gasolin",
+			"Haifeng Li",
+			"Lin Yang",
+			"MagicIon"
+	};
+	
+	private void setAdapter(){
+		ListAdapter adapter= new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1,
+				note_array
+				);
+		setListAdapter(adapter);
+	}
+
+	
+}
